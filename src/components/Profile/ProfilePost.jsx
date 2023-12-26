9843c9326bf02f5d1259cd7f38fdd03db735078e
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2023-12-26 14:19:50 +0700

    Profile Page fix

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,6 +1,6 @@
 import {
     Flex, GridItem, Image, Text, useDisclosure, Modal, ModalOverlay,
-    ModalContent, ModalCloseButton, ModalBody, Box, Avatar, Divider, VStack
+    ModalContent, ModalCloseButton, ModalBody, Button, Avatar, Divider, VStack
 } from "@chakra-ui/react";
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
@@ -57,27 +57,50 @@ export default function ProfilePost({ img }) {
                 <ModalContent>
                     <ModalCloseButton />
                     <ModalBody bg={"black"} pb={5}>
-                        <Flex gap={4} w={{ base: "90%", sm: "70%", md: "full" }} mx={"auto"}>
-                            <Box
+                        <Flex
+                            gap='4'
+                            w={{ base: "90%", sm: "70%", md: "full" }}
+                            mx={"auto"}
+                            maxH={"90vh"}
+                            minH={"50vh"}
+                        >
+                            <Flex
                                 borderRadius={4}
+                                overflow={"hidden"}
                                 border={"1px solid"}
                                 borderColor={"whiteAlpha.300"}
-                                overflow={"hidden"}
                                 flex={1.5}
+                                justifyContent={"center"}
+                                alignItems={"center"}
                             >
-                                <Image src={img} alt="user uploads" w={"100%"} h={"100%"} objectFit={"cover"}></Image>
-                            </Box>
-                            <Flex flex={1} flexDir={"column"} px={10} display={{ base: "none", md: "plex" }}
+                                <Image src={img} alt='profile post' />
+                            </Flex>
+                            <Flex flex={1} flexDir={"column"} px={10} display={{ base: "none", md: "flex" }}
                             >
-                                <Flex justifyContent={"space-between"} alignItems={"center"}>
-                                    <Flex alignItems={"center"}>
-                                        <Avatar src="/profilepic.png" size={"sm"} name="post_owner"></Avatar>
-                                        <Text fontSize={12} fontWeight={"bold"}>post_owner</Text>
+                                <Flex alignItems={"center"} justifyContent={"space-between"}>
+                                    <Flex alignItems={"center"} gap={4}>
+                                        <Avatar src={"/profilepic.png"} size={"sm"} name='As a Programmer' />
+                                        <Text fontWeight={"bold"} fontSize={12}>
+                                            {"post_owner"}
+                                        </Text>
+                                        {/* <Avatar src="/profilepic.png" size={"sm"} name="post_owner"></Avatar>
+                                        <Text fontSize={12} fontWeight={"bold"}>post_owner</Text> */}
                                     </Flex>
-                                    <Box borderRadius={4} p={1}
+                                    <Button
+                                        size={"sm"}
+                                        bg={"transparent"}
+                                        _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+                                        borderRadius={4}
+                                        p={1}
+                                    // onClick={handleDeletePost}
+                                    // isLoading={isDeleting}
+                                    >
+                                        <MdDelete size={20} cursor='pointer' />
+                                    </Button>
+                                    {/* <Box borderRadius={4} p={1}
                                         _hover={{ bg: "whiteAlpha.300", color: "red.600" }}>
                                         <MdDelete cursor={"pointer"} size={20}></MdDelete>
-                                    </Box>
+                                    </Box> */}
                                 </Flex>
                                 <Divider my={4} bg={"gray.500"} />
                                 <VStack w='full' alignItems={"start"} maxH={"350px"} overflowY={"auto"}>
@@ -91,6 +114,54 @@ export default function ProfilePost({ img }) {
                                         createdAt="4d ago"
                                         username="user_comment_2"
                                         profilePic="./profilepic.png"
+                                        text="good!"
+                                    ></Comment>
+                                    <Comment
+                                        createdAt="3d ago"
+                                        username="user_comment_2"
+                                        profilepic="./profilepic.png"
+                                        text="good!"
+                                    ></Comment>
+                                    <Comment
+                                        createdAt="3d ago"
+                                        username="user_comment_2"
+                                        profilepic="./profilepic.png"
+                                        text="good!"
+                                    ></Comment>
+                                    <Comment
+                                        createdAt="3d ago"
+                                        username="user_comment_2"
+                                        profilepic="./profilepic.png"
+                                        text="good!"
+                                    ></Comment>
+                                    <Comment
+                                        createdAt="3d ago"
+                                        username="user_comment_2"
+                                        profilepic="./profilepic.png"
+                                        text="good!"
+                                    ></Comment>
+                                    <Comment
+                                        createdAt="3d ago"
+                                        username="user_comment_2"
+                                        profilepic="./profilepic.png"
+                                        text="good!"
+                                    ></Comment>
+                                    <Comment
+                                        createdAt="3d ago"
+                                        username="user_comment_2"
+                                        profilepic="./profilepic.png"
+                                        text="good!"
+                                    ></Comment>
+                                    <Comment
+                                        createdAt="3d ago"
+                                        username="user_comment_2"
+                                        profilepic="./profilepic.png"
+                                        text="good!"
+                                    ></Comment>
+                                    <Comment
+                                        createdAt="3d ago"
+                                        username="user_comment_2"
+                                        profilepic="./profilepic.png"
                                         text="good!"
                                     ></Comment>
                                     <Comment
