e08a9ae5bb48207d63aba44c6dbe1e8a95bf2f36
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2023-12-29 15:27:40 +0700

    Feat: Suggested users

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,22 +1,17 @@
-import { VStack, Box, Flex, Text } from "@chakra-ui/react";
+import { VStack, Box, Flex, Text, Spinner } from "@chakra-ui/react";
 import SuggestedHeader from "./SuggestedHeader";
 import SuggestedUser from "./SuggestedUser";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 
 export default function SuggestedUsers() {
-    const suggestedUsers = [
-        {
-            id: 1, name: "suggest-1", followers: 1000, avatar: "./img3.png"
-        },
-        {
-            id: 2, name: "suggest-2", followers: 0, avatar: "./img2.png"
-        },
-    ]
+    const { isLoading, suggestedUsers } = useGetSuggestedUsers()
+
     return (
         <VStack py={8} px={6} gap={4}
             display={{ base: "none", md: "block" }}
         >
             <SuggestedHeader />
-            {suggestedUsers.length !== 0 && (
+            {suggestedUsers.length !== 0 && !isLoading && (
                 <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
                     <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
                         Suggested for you
@@ -27,9 +22,13 @@ export default function SuggestedUsers() {
                 </Flex>
             )}
 
-            {suggestedUsers.map((user) => (
-                <SuggestedUser user={user} key={user.id} />
-            ))}
+            {isLoading ? <Spinner /> :
+                suggestedUsers.map((user) => {
+                    return (
+                        <SuggestedUser user={user} key={user.id} setUser={() => { }} />
+                    )
+                })
+            }
 
             <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
                 © 2023 Built By{" "} T
